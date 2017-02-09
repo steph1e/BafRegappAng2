f9897cb8f74925752238e9be3d26bbefc544ecf9
steph1e <s.bontsi@gmail.com>
2017-02-09 12:52:55 +0200

    member

---
+++ src/app/components/members/member.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'member',
+  templateUrl: './member.component.html',
+  styleUrls: ['./member.component.css']
+})
+export class MemberComponent { }

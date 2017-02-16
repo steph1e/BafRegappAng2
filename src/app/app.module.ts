072a91f8c88cab034925c4f4efdfe418c28c9479
steph1e <s.bontsi@gmail.com>
2017-02-16 14:17:14 +0200

    form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -37,7 +37,8 @@ const appRoutes: Routes = [
     FormsModule,
     HttpModule,
     RouterModule.forRoot(appRoutes),
-    //routing
+    //routing,
+    ReactiveFormsModule
   ],
   providers: [MembersService],
   bootstrap: [AppComponent]
--- src/app/components/add-member/add-member.component.ts
+++ src/app/components/add-member/add-member.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Member } from '../../member'
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-add-member',
@@ -8,16 +10,21 @@ import { Member } from '../../member'
 })
 export class AddMemberComponent implements OnInit {
 
- 
-  model = new Member();
+ public myForm: FormGroup; // our model driven form
+    public submitted: boolean; // keep track on whether form is submitted
+    public events: any[] = []; // use later to display form changes
 
-  submitted = false;
-
-  onSubmit() { this.submitted = true; }
-
-  constructor() { }
+  constructor(private _fb: FormBuilder) { }
 
   ngOnInit() {
   }
+
+save(model: Member, isValid: boolean) {
+        this.submitted = true; // set form submit to true
+
+        // check if model is valid
+        // if valid, call API to save customer
+        console.log(model, isValid);
+    }
 
 }
